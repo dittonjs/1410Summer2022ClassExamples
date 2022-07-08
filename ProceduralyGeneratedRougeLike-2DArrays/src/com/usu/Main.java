16dd8ca642b496f65eb8b2dd5bf64ede167f0802
Joseph Ditton <jditton.atomic@gmail.com>
2022-07-08 10:06:00 -0600

    done!

--- ProceduralyGeneratedRougeLike-2DArrays/src/com/usu/Main.java
+++ ProceduralyGeneratedRougeLike-2DArrays/src/com/usu/Main.java
@@ -1,7 +1,9 @@
 package com.usu;
 
+import javax.management.relation.RelationNotFoundException;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     // P = player
@@ -10,22 +12,150 @@ public class Main {
     // K = key
     // D = door
     public static void main(String[] args) {
-        char[][] map = new char[40][];
+        char[][] map = generateLevel();
+        Scanner scanner = new Scanner(System.in);
+        boolean hasKey = false;
+        boolean isAlive = true;
+        while(isAlive) {
+            displayMap(map);
+            String input = scanner.next();
+            if (input.equals("done")) {
+                break;
+            }
+            int result = movePlayer(map, input);
+            if (result == 1) {
+                hasKey = true;
+            }
+            if (result == 2 && hasKey) {
+                map = generateLevel();
+                hasKey = false;
+                continue;
+            }
+            isAlive = !moveMonsters(map);
+        }
+        System.out.println("You ded bro!");
+    }
+
+    private static int movePlayer(char[][] map, String input) {
+        int nextRow = 0;
+        int nextCol = 0;
+        for (int row = 0; row < map.length; row ++) {
+            for (int col = 0; col < map.length; col ++) {
+                if (map[row][col] == 'P') {
+                    if (input.equals("w")) {
+                        nextRow = row - 1;
+                        nextCol = col;
+                    }
+                    if (input.equals("a")) {
+                        nextRow = row;
+                        nextCol = col - 1;
+                    }
+                    if (input.equals("s")) {
+                        nextRow =  row + 1;
+                        nextCol = col;
+                    }
+                    if (input.equals("d")) {
+                        nextRow = row;
+                        nextCol = col + 1;
+                    }
+                    // key = 1
+                    // door = 2
+                    // everything else = 0
+                    if (map[nextRow][nextCol] == ' ') {
+                        map[row][col] = ' ';
+                        map[nextRow][nextCol] = 'P';
+                        return 0;
+                    }
+                    if (map[nextRow][nextCol] == 'K') {
+                        map[row][col] = ' ';
+                        map[nextRow][nextCol] = 'P';
+                        return 1;
+                    }
+                    if (map[nextRow][nextCol] == 'D') {
+                        return 2;
+                    }
+                    break;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static boolean moveMonsters(char[][] map) {
+        boolean foundPlayer = false;
+        for (int row = 0; row < map.length; row ++) {
+            for (int col = 0; col < map.length; col ++) {
+                if (map[row][col] == 'M') {
+                    int nextRow = row;
+                    int nextCol = col;
+                    if (map[row][col + 1] == 'P') {
+                        foundPlayer = true;
+                        nextCol = col + 1;
+                    } else if(map[row][col - 1] == 'P') {
+                        foundPlayer = true;
+                        nextCol = col - 1;
+                    } else if (map[row + 1][col] == 'P') {
+                        foundPlayer = true;
+                        nextRow = row + 1;
+                    } else if (map[row - 1][col] == 'P') {
+                        foundPlayer = true;
+                        nextRow = row - 1;
+                    } else {
+                        Random random = new Random();
+
+                        if (random.nextDouble() < .5) {
+                            if (random.nextDouble() < .5) {
+                                nextRow = row - 1;
+                            } else {
+                                nextRow = row + 1;
+                            }
+                        } else {
+                            if (random.nextDouble() < .5) {
+                                nextCol = col - 1;
+                            } else {
+                                nextCol = col + 1;
+                            }
+                        }
+                    }
+                    if (map[nextRow][nextCol] == ' ' || map[nextRow][nextCol] == 'P') {
+                        map[row][col] = ' ';
+                        map[nextRow][nextCol] = 'm';
+                    }
+                }
+            }
+        }
+        for (int row = 0; row < map.length; row ++) {
+            for (int col = 0; col < map.length; col ++) {
+                if (map[row][col] == 'm') {
+                    map[row][col] = 'M';
+                }
+            }
+        }
+        return foundPlayer;
+    }
+
+    private static char[][] generateLevel() {
+        char[][] map = new char[12][];
         fillMap(map);
         createWalls(map);
         generateInnerWalls(map);
         map = doCellularAutomata(map);
-        displayMap(map);
+        addStaticElements(map);
+        randomlyPlaceElement(map, 'K');
+        randomlyPlaceElement(map, 'M');
+        randomlyPlaceElement(map, 'M');
+        randomlyPlaceElement(map, 'M');
+        return map;
     }
 
-    public static void fillMap(char[][] map) {
+    private static void fillMap(char[][] map) {
         for (int i = 0; i < map.length; i++) {
-            map[i] = new char[40];
+            map[i] = new char[12];
             Arrays.fill(map[i], ' ');
         }
     }
 
-    public static void createWalls(char[][] map) {
+    private static void createWalls(char[][] map) {
         Arrays.fill(map[0], '*');
         Arrays.fill(map[map.length - 1], '*');
         for (int i = 0; i < map.length; i++) {
@@ -34,7 +164,7 @@ public class Main {
         }
     }
 
-    public static void generateInnerWalls(char[][] map) {
+    private static void generateInnerWalls(char[][] map) {
         Random random = new Random();
         for (int i = 2; i < map.length - 2; i++) {
             for (int j = 2; j < map[i].length - 2; j ++) {
@@ -44,7 +174,7 @@ public class Main {
             }
         }
     }
-    public static char[][] copyMap(char[][] map) {
+    private static char[][] copyMap(char[][] map) {
         char[][] newMap = new char[map.length][];
         for (int i = 0; i < map.length; i++) {
             newMap[i] = new char[map[i].length];
@@ -52,7 +182,7 @@ public class Main {
         }
         return newMap;
     }
-    public static char[][] doCellularAutomata(char[][] map) {
+    private static char[][] doCellularAutomata(char[][] map) {
         char[][] mapCopy = null;
         for (int k = 0; k < 40; k ++) {
             mapCopy = copyMap(map);
@@ -71,7 +201,7 @@ public class Main {
         return mapCopy;
     }
 
-    public static int countNumberOfNeighbors(char[][] map, int row, int col) {
+    private static int countNumberOfNeighbors(char[][] map, int row, int col) {
         int count = 0;
         if (map[row - 1][col - 1] == '*') count ++;
         if (map[row - 1][col] == '*') count ++;
@@ -84,7 +214,23 @@ public class Main {
         return count;
     }
 
-    public static void displayMap(char[][] map) {
+    private static void addStaticElements(char[][] map) {
+        map[map.length - 2][1] = 'P';
+        map[1][map[1].length - 2] = 'D';
+    }
+
+    private static void randomlyPlaceElement(char[][] map, char element) {
+        Random random = new Random();
+        int row = random.nextInt(map.length - 3) + 1;
+        int col = random.nextInt(map[0].length - 3) + 1;
+
+        while(map[row][col] != ' ') {
+            row ++;
+        }
+        map[row][col] = element;
+    }
+
+    private static void displayMap(char[][] map) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j ++) {
                 System.out.print(map[i][j] + " ");
